69fdea041a813187377dc7ab9ac74db78f080239
ravi <ravikanth.koraveni@zemosolabs.com>
2018-04-24 16:54:27 +0530

    include sub directory search

--- eclipse-workspace/assignments/src/assignment1/Assignment1.java
+++ eclipse-workspace/assignments/src/assignment1/Assignment1.java
@@ -13,27 +13,37 @@ public static void main(String[] args) {
 		
 		System.out.println("Enter the regular expression");
 		givenRegex = scan.next();
-		File dir = new File("/home/zemoso");
-		FilenameFilter filter  = new FilenameFilter(){
-			public boolean accept(File dir,String name) {
-				return name.matches(givenRegex);
-			}
-		};
-		String[] files = dir.list(filter);
-		if(files==null) {
-			System.out.println("No files matching the given regex");
-		} else {
-			System.out.println(files.length);
-			for(int i=0;i<files.length;i++) {
-				String fileName = files[i];
-				System.out.println(fileName+"   "+new File(fileName).getAbsolutePath());
-			}
-		}
-		
+		printMatching(new File("/home/zemoso"),givenRegex);
 		System.out.println("to continue enter 1 or 0 to stop");
 		check=scan.nextInt();
 	}
 	scan.close();
 	
+}
+private static void printMatching(File dir,String givenRegex) {
+	FilenameFilter subDirs = new FilenameFilter() {
+		public boolean accept(File dir,String name) {
+			return new File(dir,name).isDirectory();
+		}
+	};
+	File[] subDirectories = dir.listFiles(subDirs);
+	FilenameFilter filter  = new FilenameFilter(){
+		public boolean accept(File dir,String name) {
+			return name.matches(givenRegex);
+		}
+	};
+	String[] files = dir.list(filter);
+	if(files==null) {
+	} else {
+		for(int i=0;i<files.length;i++) {
+			String fileName = files[i];
+			System.out.println(fileName+"   "+new File(fileName).getAbsolutePath());
+		}
+	}
+	if(subDirectories !=null) {
+		for(int i=0;i<subDirectories.length;i++) {
+			printMatching(subDirectories[i],givenRegex);
+		}
+	}
 }
 }
