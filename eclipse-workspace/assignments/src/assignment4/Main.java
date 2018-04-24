8451bc783f6af2abdb48a3acc2d4c8acfa2b2352
ravi <ravikanth.koraveni@zemosolabs.com>
2018-04-25 00:24:36 +0530

    tried to optimize again

--- eclipse-workspace/assignments/src/assignment4/Main.java
+++ eclipse-workspace/assignments/src/assignment4/Main.java
@@ -2,7 +2,13 @@ package assignment4;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Main {
@@ -24,63 +30,27 @@ private static void printRange(String signUpDate, String currDate) throws ParseE
 		System.out.println("No Range");
 		return;
 	}
-	int signYear,signMonth,signDay;
-	String[] sign = signUpDate.split("-");
-	signYear = Integer.parseInt(sign[2]);
-	signMonth = Integer.parseInt(sign[1]);
-	signDay = Integer.parseInt(sign[0]);
-	//getting the current date
-	int currYear,currMonth,currDay;
-	String[] curr = currDate.split("-");
-	currYear = Integer.parseInt(curr[2]);
-	currMonth = Integer.parseInt(curr[1]);
-	currDay = Integer.parseInt(curr[0]);
-	
-	//number of days in the month
-	int[] monthDays = {31,28,31,30,31,30,31,31,30,31,30,31};
-	//checking for leap years
-	if(currYear%4==0) {
-		monthDays[1]=29;
+	//extracting year from given date
+	Instant instant = currentDate.toInstant();
+	ZonedDateTime zdt = instant.atZone(ZoneId.systemDefault());
+	LocalDate testDate = zdt.toLocalDate();
+	int cyear = testDate.getYear();
+	//creating and updating the anniversary Date
+	Calendar aCal = Calendar.getInstance();
+	aCal.setTime(signDate);
+	aCal.set(Calendar.YEAR,cyear);
+	//finding the range
+	aCal.add(Calendar.DATE,30);
+	Date preDate = aCal.getTime();
+	aCal.add(Calendar.DATE,-60);
+	Date postDate = aCal.getTime();
+	if(currentDate.before(preDate)){
+		preDate = currentDate;
 	}
-	int postDate,postMonth,postYear=currYear,preDate,preMonth,preYear;
-	postDate = 30-signDay;
-	if(postDate==0) {
-		postMonth= signMonth-1;
-		if(postMonth==0) {
-			postYear=currYear-1;
-		}
-		postDate = monthDays[postMonth-1];
-	}else if(postDate<0) {
-		postMonth = signMonth;
-		postDate = -1*postDate;
-	}else {
-		postMonth= signMonth-1;
-		if(postMonth==0) {
-			postYear=currYear-1;
-		}
-		postDate = monthDays[postMonth-1]-postDate;
-	}
-	
-	preDate = monthDays[signMonth-1]-signDay;
-	preDate = 30-preDate;
-	if(monthDays[signMonth+1-1]<preDate) {
-		preMonth = signMonth+2;
-		preDate = preDate-monthDays[signMonth+1-1];
 
-	}else {
-		preMonth = signMonth+1;
-	}
-	preYear = currYear;
-	if(preMonth>currMonth) {
-		preDate = currDay;
-		preMonth = currMonth;
-		preYear = currYear;
-	}
-	if(preMonth==currMonth && preDate>currDay) {
-		preDate = currDay;
-	}
-	System.out.println(postDate+"-"+postMonth+"-"+postYear+" "+preDate+"-"+preMonth+"-"+preYear);
-	
+	System.out.println(dateFormat.format(postDate)+" "+dateFormat.format(preDate));
+
+
 }
 
 
